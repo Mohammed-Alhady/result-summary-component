d23c58a01a01cdd6ec7fb4b3ca41b9412c467cb7
Mohammed Alhady <mohammedalhady002@gmail.com>
2023-09-10 15:01:10 +0200

    create a new challenge by frontend mentor

---
+++ src/main.js
@@ -0,0 +1,22 @@
+fetch("../date/data.json")
+    .then((result) => {
+        const myData = result.json();
+        return myData;
+    })
+    .then((myData) => {
+        myData.forEach(element => {
+            createElement(element);
+        });
+    })
+
+function createElement(element) {
+    const categories = document.querySelectorAll(".details li");
+    categories.forEach((ele) => {
+        if (ele.type === element.category) {
+            ele.children[0].src = element.icon;
+            ele.children[1].innerHTML = element.category;
+            ele.children[2].children[0].innerHTML = element.score;
+            ele.style.background = element.background;
+        }
+    })
+}
